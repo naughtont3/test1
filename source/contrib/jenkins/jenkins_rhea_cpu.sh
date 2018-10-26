00721b1298a90b7cd92740b967b0c76552e5388a
Thomas Naughton <naughtont@ornl.gov>
2018-10-26 15:45:16 -0400

    change the ending logic for job success/fail

--- source/contrib/jenkins/jenkins_rhea_cpu.sh
+++ source/contrib/jenkins/jenkins_rhea_cpu.sh
@@ -16,7 +16,7 @@ cat > $BUILD_TAG.pbs << EOF
 # Arg1 retval
 # Arg2 err-string
 die () {
-    echo "ERROR: \$2"
+    echo "ERROR: \$2 (rc=\$1)"
     exit \$1
 }
 
@@ -116,13 +116,30 @@ cp $BUILD_DIR/$BUILD_TAG.o* ../
 
 # explicitly check for correct test output from all builds
 #[ $(grep '100% tests passed, 0 tests failed out of [0-9]*' ../$BUILD_TAG.o* | wc -l) -eq 4 ]
+
 # TODO: IMPROVE OUR CHECK OF THE BUILD/TEST OUTPUT (see above for example)
-if [ 0 -eq $job_rc ] ; then
-    # Success
-    echo "CI-STATUS: $BUILD_TAG PASSED"
-    exit 0
-else
-    # Failed
-    echo "CI-STATUS: $BUILD_TAG FAILED"
+if [ 0 -ne $job_rc ] ; then
+    # Failed job
+    echo "CI-STATUS: $BUILD_TAG JOB-FAILED (job_rc=$job_rc)"
     exit $job_rc
 fi
+# TJN: Crudely check for 'ERROR' in the log output
+if [ $(grep 'ERROR:' ../$BUILD_TAG.o* | wc -l) -gt 0 ] ; then
+    # Failed
+    echo "CI-STATUS: $BUILD_TAG FAILED"
+    exit 1
+fi
+if [ $(grep 'error:' ../$BUILD_TAG.o* | wc -l) -gt 0 ] ; then
+    # Failed
+    echo "CI-STATUS: $BUILD_TAG FAILED"
+    exit 1
+fi
+if [ $(grep 'Error:' ../$BUILD_TAG.o* | wc -l) -gt 0 ] ; then
+    # Failed
+    echo "CI-STATUS: $BUILD_TAG FAILED"
+    exit 1
+fi
+
+# Success
+echo "CI-STATUS: $BUILD_TAG PASSED"
+exit 0
