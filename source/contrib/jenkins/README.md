d5fcbed2c56e2a94f85832235f71b4a083a93d0a
Thomas Naughton <naughtont@ornl.gov>
2018-10-26 10:09:04 -0400

    add contrib dir with jenkins bits

---
+++ source/contrib/jenkins/README.md
@@ -0,0 +1,12 @@
+README
+------
+
+Various items for Jenkins CI testing.
+
+
+NOTES
+-----
+ - The `jenkins_rhea_cpu.sh` script was modified from the example by
+   GrahamL and ScottK.  Specifically I used the version from the QMCPACK
+   repo, [https://raw.githubusercontent.com/QMCPACK/qmcpack/2f6bb1f8646d9176e851c36780cd9d3a9eb6b5a8/tests/test_automation/jenkins_rhea_cpu.sh](https://raw.githubusercontent.com/QMCPACK/qmcpack/2f6bb1f8646d9176e851c36780cd9d3a9eb6b5a8/tests/test_automation/jenkins_rhea_cpu.sh).
+
---
+++ source/contrib/jenkins/jenkins_rhea_cpu.sh
@@ -0,0 +1,127 @@
+#!/bin/bash -x
+
+BUILD_DIR=$(pwd)
+echo $BUILD_DIR
+
+cat > $BUILD_TAG.pbs << EOF
+#PBS -A CSC040
+#PBS -N $BUILD_TAG
+#PBS -j oe
+#PBS -l walltime=1:00:00,nodes=1
+#PBS -d $BUILD_DIR
+#PBS -l partition=rhea
+
+echo "================================================="
+echo -n "   Started test at: "
+date
+echo "================================================="
+
+cd $BUILD_DIR
+
+module unload PE-intel
+module unload openmpi
+
+module load PE-gnu/5.3.0-1.10.2
+module load git
+
+# Load/Show our Autotools last
+source /ccs/home/csc040ci_auser/local/env_gnu.sh
+which m4
+which autoconf
+which automake
+which libtool
+
+
+echo "# INFO: ENVIRONMENT (--begin--)"
+env
+module list
+echo "# INFO: ENVIRONMENT (-- end --)"
+
+echo "# INFO: HOST"
+hostname
+
+echo "# INFO: DATE"
+date
+
+echo "# INFO: PWD (START)"
+pwd
+
+echo ""
+echo ""
+echo "starting new test"
+echo ""
+echo ""
+
+cd source/
+
+mkdir -p build
+cd build
+
+echo "# DBG: PWD (BUILD)"
+pwd
+
+rc=0
+
+echo "# DBG: Run autogen.sh"
+../autogen.sh
+rc=$?
+if [ 0 -ne $rc ] ; then
+  echo "autogen.sh failed. exiting."
+  exit 1
+fi
+
+echo "# DBG: Run configure"
+./configure
+rc=$?
+if [ 0 -ne $rc ] ; then
+  echo "configure failed. exiting."
+  exit 2
+fi
+
+echo "# DBG: Run make"
+make
+rc=$?
+if [ 0 -ne $rc ] ; then
+  echo "make failed. exiting."
+  exit 3
+fi
+
+# TODO: Add make install
+# TODO: Add make check
+
+echo "# INFO: SUCCESS. exiting."
+
+echo "================================================="
+echo -n "  Finished test at: "
+date
+echo "================================================="
+
+exit 0
+EOF
+
+/home/csc040ci_auser/blocking_qsub $BUILD_DIR $BUILD_TAG.pbs
+job_rc=$?
+
+echo "================================================="
+echo " JOB_RC=$job_rc"
+echo "================================================="
+
+cp $BUILD_DIR/$BUILD_TAG.o* ../
+
+## this end of job logic could probably be more elegant
+## hacks to get us going
+
+cp $BUILD_DIR/$BUILD_TAG.o* ../
+
+# explicitly check for correct test output from all builds
+#[ $(grep '100% tests passed, 0 tests failed out of [0-9]*' ../$BUILD_TAG.o* | wc -l) -eq 4 ]
+# TODO: IMPROVE OUR CHECK OF THE BUILD/TEST OUTPUT (see above for example)
+if [ 0 -eq $job_rc ] ; then
+    # Success
+    echo "CI-STATUS: $BUILD_TAG PASSED"
+    exit 0
+else
+    # Failed
+    echo "CI-STATUS: $BUILD_TAG FAILED"
+    exit $job_rc
+fi
